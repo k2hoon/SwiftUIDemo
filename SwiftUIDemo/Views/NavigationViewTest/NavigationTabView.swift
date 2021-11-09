7b651b25d9a18a05ada330ba5bc5e0374f59c3b0
Kyung Hoon Kim <tayesymbol@gmail.com>
2021-11-10 02:03:28 +0900

    Add property control test by moving view stack with navigation view in tab view.

--- SwiftUIDemo/Views/NavigationViewTest/NavigationTabView.swift
+++ SwiftUIDemo/Views/NavigationViewTest/NavigationTabView.swift
@@ -9,31 +9,47 @@ import SwiftUI
 
 struct NavigationTabView: View {
     enum Tab {
-        case view1
-        case view2
-        case view3
+        case tab1
+        case tab2
+        case tab3
     }
     
-    @State var selection: Tab = .view1
-    @State var isBarHidden = false
+    @State var navBarHidden = false
+    @State var tabSelection: Tab = .tab1
     
     var body: some View {
         NavigationView {
-            TabView(selection: $selection) {
-                NaviTestView()
-                    .tabItem { Text("view1") }
-                    .tag(Tab.view1)
-                NaviTestView1()
-                    .tabItem { Text("view2") }
-                    .tag(Tab.view2)
-                NaviTestView2()
-                    .tabItem { Text("view3") }
-                    .tag(Tab.view3)
+            TabView(selection: $tabSelection) {
+                NavigationTabView1(navBarHidden: $navBarHidden)
+                    .tabItem { Text("Tab view 1") }
+                    .tag(Tab.tab1)
+                
+                NavigationTabView2(navBarHidden: $navBarHidden)
+                    .tabItem { Text("Tab view 2") }
+                    .tag(Tab.tab2)
+                
+                NavigationTabView3(navBarHidden: $navBarHidden)
+                    .tabItem { Text("Tab view 3") }
+                    .tag(Tab.tab3)
             }
-            .navigationBarHidden(self.isBarHidden)
+            .navigationTitle(getTabTitle(tab: self.tabSelection))
         }
-        .onAppear {
-            self.isBarHidden = true
+        .onAppear(perform: {
+            self.navBarHidden = true
+        })
+    }
+    
+    /// Returns an appropriate title string for each selected tab.
+    /// - Parameter tab: tab selection
+    /// - Returns: navigation title
+    func getTabTitle(tab: Tab) -> String {
+        switch tab {
+        case .tab1:
+            return "NavigationTabView1"
+        case .tab2:
+            return "NavigationTabView2"
+        case .tab3:
+            return "NavigationTabView3"
         }
     }
 }
@@ -44,20 +60,43 @@ struct NavigationTabView_Previews: PreviewProvider {
     }
 }
 
-struct NaviTestView: View {
+struct NavigationTabView1: View {
+    @Binding var navBarHidden : Bool
     var body: some View {
         ZStack {
             Color.red
-            
-            VStack {
-                Text("Hello")
+            NavigationLink(destination: NavigationTabDetailView1()) {
+                Text("Go to tab detail view")
+                    .foregroundColor(.black)
+                    .padding()
+                    .border(Color.blue)
             }
         }
-        .hiddenNavigationBar()
+        .navigationBarHidden(self.navBarHidden)
+        .onAppear() {
+            print("NavigationTabView2::onAppear")
+            self.navBarHidden = false
+        }
+    }
+    
+    struct NavigationTabDetailView1: View {
+        var body: some View {
+            ZStack {
+                Color.red
+                VStack {
+                    Text("Hello")
+                }
+                .navigationTitle("NavigationTabDetailView1")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+        }
     }
 }
 
-struct NaviTestView1: View {
+
+struct NavigationTabView2: View {
+    @Binding var navBarHidden : Bool
+    
     var body: some View {
         ZStack {
             Color.blue
@@ -65,33 +104,35 @@ struct NaviTestView1: View {
                 Text("Hello")
             }
         }
-        .hiddenNavigationBar()
+        .navigationBarHidden(self.navBarHidden)
+        .onAppear() {
+            print("NavigationTabView2::onAppear")
+            self.navBarHidden = false
+        }
     }
 }
 
 
-struct NaviTestView2: View {
+struct NavigationTabView3: View {
+    @Binding var navBarHidden : Bool
+    
     var body: some View {
         ZStack {
             Color.green
-            VStack {
-//                ScrollView {
-//                    ForEach(0..<50) { value in
-//                        Text("\(value)")
-//                    }
-//                }
-//                .padding()
-                
-                List {
-                    ForEach(0..<50) { value in
-                        Text("\(value)")
-                    }
-                    .listRowInsets(EdgeInsets())
+            List {
+                ForEach(0..<50) { value in
+                    Text("\(value)")
                 }
-                .listStyle(PlainListStyle())
-                .padding()
+                .listRowInsets(EdgeInsets())
             }
+            .listStyle(PlainListStyle())
+            .padding()
         }
-        .hiddenNavigationBar()
+        .navigationBarHidden(self.navBarHidden)
+        .onAppear() {
+            print("NavigationTabView3::onAppear")
+            self.navBarHidden = true
+        }
+//        .hiddenNavigationBar()
     }
 }
