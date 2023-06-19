081c7e08830f93967b770f600a2bc49c374094da
Kyung Hoon Kim <tayesymbol@gmail.com>
2023-06-20 00:31:44 +0900

    Update button test view (#32)

--- SwiftUIDemo/Views/BaseCollection/BasicTest/ButtonTestView.swift
+++ SwiftUIDemo/Views/BaseCollection/BasicTest/ButtonTestView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ButtonTestView: View {
     var body: some View {
-        Text("Button test")
+        VStack {
+            ButtonDefault()
+            
+            ButtonBorder()
+            
+            ButtonClipshape()
+            
+            Spacer()
+        }
     }
 }
 
@@ -17,4 +25,111 @@ struct ButtonTestView_Previews: PreviewProvider {
     static var previews: some View {
         ButtonTestView()
     }
+}
+
+// MARK: ButtonTestView extension
+extension ButtonTestView {
+    struct ButtonDefault: View {
+        var body: some View {
+            VStack {
+                Text("Default")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                
+                HStack {
+                    
+                    Button("Button", action: {})
+                        .padding()
+                    
+                    Button("Button", action: {})
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                    
+                    Button("Button", action: {})
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(16)
+                }
+                
+                Divider()
+            }
+        }
+    }
+    
+    struct ButtonBorder: View {
+        var body: some View {
+            VStack {
+                Text("Button border and stroke")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                
+                HStack {
+                    Button("Button", action: {})
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .border(.red, width: 4)
+                    
+                    Button(action: {}) {
+                        Text("Button")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .strokeBorder(Color.red, lineWidth: 4)
+                            )
+                    }
+                    
+                    Button(action: {}) {
+                        Text("Button")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.red, lineWidth: 4)
+                            )
+                    }
+                }
+                
+                Divider()
+            }
+        }
+    }
+    
+    struct ButtonClipshape: View {
+        var body: some View {
+            VStack {
+                Text("Button clipshape")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                
+                HStack {
+                    Button("Button", action: {})
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                    
+                    Button("Button", action: {})
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .clipShape(Circle())
+                }
+                
+                Divider()
+            }
+        }
+    }
+    
 }
